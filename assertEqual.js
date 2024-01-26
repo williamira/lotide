3f10c9e846d102802e6e5d1722b924fdbf2bf3db
williamira <labber@Zeus>
2024-01-26 10:30:58 -0700

    updated assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟩🟢🟩 Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`🟩🟢🟩 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("⛔🛑🚫 Assertion Failed: " + [actual] + " !== " + [expected]);
+    console.log(`⛔🛑🚫 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
