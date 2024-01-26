a7400111ec003ad97cd37f812b2cd92f60c48327
williamira <labber@Zeus>
2024-01-26 10:12:36 -0700

    assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟩🟢🟩 Assertion Passed: " + [actual] + " === " + [expected]);
+  } else {
+    console.log("⛔🛑🚫 Assertion Failed: " + [actual] + " !== " + [expected]);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("I am marvelous", "Nope");
+assertEqual(2, 22);
+assertEqual("Life", "Life");
