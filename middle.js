0a0234ccbe075e4f743c92ba3d74ba8016163e8b
williamira <williamira22@gmail.com>
2024-01-28 12:09:03 -0700

    middle function for retriving specific index of array

--- middle.js
+++ middle.js
@@ -17,3 +17,23 @@ const assertArrayEqual = function(array1, array2) {
     console.log(`Assertion failed🚫🚫🚫 ${array1} is not equal to ${array2}`);
   }
 };
+
+const middle = function(array) {
+  let newArr = [];
+    if (array.length < 3) {
+      return []; // returns nothing if the array has 1 or 2 indexes
+    } 
+    else if (array.length % 2 !== 0) {
+      return [array[(array.length - 1) / 2]]; //push middle element for odd array
+    }
+    else {
+      return [array[array.length / 2 - 1], array[array.length / 2]];
+    }
+  };
+
+assertArrayEqual(middle([1, 2, 3]), [2]);
+assertArrayEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+assertArrayEqual(middle([]), []); 
+assertArrayEqual(middle(["hello", "world"]), []); 
+assertArrayEqual(middle(["howdy", "partner", "looking"]), ["partner"]); 
+
