f77346be9004bd7f6c1fe1e5e389333bba2fea56
williamira <williamira22@gmail.com>
2024-02-01 22:16:10 -0700

    assertObjectsEqual Function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -33,11 +33,16 @@ const eqArrays = function(arr1, arr2) {
 
 
 const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect
+  const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
     console.log(`Assertion passed🟩🟢💚 ${inspect(actual)} === ${inspect(expected)}`);
   } else {
     console.log(`Assertion failed🚫🚫🚫 ${inspect(actual)} !== ${inspect(expected)}`);
   }
-}
+};
 
+const shirtObject = { color: ["red", "blue"], size: "medium" };
+const anotherShirtObject = { size: "medium", color: ["red", "blue"] };
+
+
+assertObjectsEqual(shirtObject, anotherShirtObject);
