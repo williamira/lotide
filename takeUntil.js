05304cd4b581f9bf218c89c3063e1a900d89fa36
williamira <williamira22@gmail.com>
2024-02-03 16:54:00 -0700

    takeUntil function to slice arrays at a given value

--- takeUntil.js
+++ takeUntil.js
@@ -18,4 +18,31 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    }
+    results.push(item);
+  }
+  return results;
+};
 
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+const data3 = [];
+const results3 = takeUntil(data3, x => x === '')
+
+const data4 = [undefined]
+const results4 = takeUntil(data4, x => x === undefined)
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+assertArraysEqual(results3, []);
+assertArraysEqual(results4, [])
