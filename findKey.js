89141351d7eb90a3210ad2cf3023d63de679df8e
williamira <williamira22@gmail.com>
2024-02-03 17:20:22 -0700

    findKey function that iterates through objects and returns key

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩🟢🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔🛑🚫 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined
+};
+
+const peopleAge = {
+  William: 29,
+  John: 33,
+  Sarah: 44,
+  Lucy: 57
+};
+
+const randomObject = {
+  roses: "Red",
+  violets: "blue",
+  lilies: "orange"
+};
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
+assertEqual(findKey({}, x => x === 22), undefined)
+assertEqual(findKey(peopleAge, x => x > 30), "John")
+assertEqual(findKey(randomObject, x => x === "purple"), undefined)
