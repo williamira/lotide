3ecb9fd71fa1a9876ebe736d1a4125b971e46d3c
williamira <williamira22@gmail.com>
2024-02-14 20:27:28 -0700

    Drying up assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,9 +2,9 @@ const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) { // calls eqArray function to determine if two arrays have the same elements
-    console.log(`Assertion passed: ${array1} is equal to ${array2}`); // if true then prints this message to console
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);; // if true then prints this message to console
   } else {
-    console.log(`Assertion failed: ${array1} is not equal to ${array2}`);// if false then prints this message to console
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);// if false then prints this message to console
   }
 };
 
