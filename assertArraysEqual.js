4bfbe73efce9a479ae704204818db2c2f8a82ef5
williamira <williamira22@gmail.com>
2024-02-14 20:25:41 -0700

    Drying up assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,11 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion passed: ${array1} is equal to ${array2}`);
+  if (eqArrays(array1, array2)) { // calls eqArray function to determine if two arrays have the same elements
+    console.log(`Assertion passed: ${array1} is equal to ${array2}`); // if true then prints this message to console
   } else {
-    console.log(`Assertion failed: ${array1} is not equal to ${array2}`);
+    console.log(`Assertion failed: ${array1} is not equal to ${array2}`);// if false then prints this message to console
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2,], [1, 2, 3]);
+module.exports = assertArraysEqual
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require('../assertArraysEqual')
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // to test is two arrays are containg equal elements 
+assertArraysEqual([1, 2,], [1, 2, 3]); // to test if we recieve a failed assertion for having two arrays with different elements
+assertArraysEqual([], []); //to test if two empty arrays pass the assertion
+assertArraysEqual(["Hello", "World"], ["hello", 'world']); // to test if arrays with very similar strings fail for capitilization
+assertArraysEqual([2], ["2"]); // To test if a number and string of that number fail assertion
