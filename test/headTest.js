7a6e2baab6e5493f3982bfa5c10d27bd6ebb2e3b
williamira <williamira22@gmail.com>
2024-02-16 20:49:59 -0700

    Refactored middle and tail functions

--- test/headTest.js
+++ test/headTest.js
@@ -4,18 +4,19 @@ const head = require('../head');
 
 
 describe("#head", () => {
+  // To test the the first element in an array of numbers is returned
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-
+  // To test if the first elemtn in an array with one element is returned
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5');
   });
-
+  // To test if first element in array of strings is returned
   it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
     assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
   });
-
+  // to test in undefined is returned in an empty array
   it("returns undefined for []", () => {
     assert.strictEqual(head([]), undefined);
   });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,17 +1,21 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const result = tail([20, 30, 40]);
-assertEqual(result.length, 2); // To test if we get back two elements
-assertEqual(result[0], 30); // to test if the element at index 0 is = 30
-assertEqual(result[1], 40); // to test if the element at index 1 is = 40
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // To test if the original array remains intact after the function is implemented
-
-const newArray = ["Hello"];
-assertEqual(tail(newArray).length, 0); // To test if there is only one element in an array that an empty array be returned
-
-const emptyArr = [];
-assertEqual(tail(emptyArr).length, 0); // testing that if there is no elements in an array that an empty element be returned
+describe("#tail", () => {
+  // To test if every element after the first one is returned in an array of numbers
+  it("returns [30, 40] for [20, 30, 40]", () => {
+    assert.deepEqual(tail([20, 30, 40]), [30, 40]);
+  });
+  // To test if every element after the first one is returned in an array of strings 
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  // To test if there is only one element in an array that an empty array be returned
+  it("returns [] for ['hello']", () => {
+    assert.deepEqual(tail(['hello']), []);
+  });
+  // testing that if there is no elements in an array that an empty element be returned
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
