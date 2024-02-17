a2de802c6774c60cc5d4589436386414ab356842
williamira <williamira22@gmail.com>
2024-02-17 13:57:13 -0700

    refactored function tests and added them each to new file.. still need to convert to chai/mocha

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten');
 const letterPositions = require('./letterPositions');
-const map = require('./map');
 const takeUntil = require('./takeUntil');
 const without = require('./without');
 
@@ -31,7 +30,6 @@ module.exports = {
   findKeyByValue: findKeyByValue,
   flatten: flatten,
   letterPositions: letterPositions,
-  map: map,
   takeUntil: takeUntil,
   without: without
 };
